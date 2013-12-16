6c283075ba790b01e1259bdf7120c141abcdb872
Bruno Freitas <bmsatierf@gmail.com>
2013-12-15 23:16:09 -0200

    Define a single page for each layout and adds layouts C and D

--- js/emulator.js
+++ js/emulator.js
@@ -31,21 +31,6 @@ $(document).ready(function() {
         return;
     }
   });
-
-  // Style options
-  var customStyle = window.location.hash;
-
-  if(customStyle.indexOf("#") > 0) {
-    $("a").removeClass("selected");
-    $("#custom-emulator").attr("href", "css/" + customStyle.substring(1) + ".css");
-    $("#custom-" + customStyle.substring(1)).addClass("selected");
-  }
-
-  $("#options li a").click(function() {
-    $("#custom-emulator").attr("href",$(this).attr('rel'));
-    $("a").removeClass("selected");
-    $(this).addClass("selected");
-  });
 });
 
 function charCode(key){
